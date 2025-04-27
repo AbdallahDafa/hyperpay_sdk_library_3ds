12388a387d168005d6800a18e2d36efcdc473751
AbdallahDafa <abdallah.mahmoud@dafa.sa>
2025-04-27 16:32:17 +0300

    1.0.32 tag   * change repository name readme

--- app/src/main/java/com/dafa/hyperpaysdk/core/TestHyperPay3Ds.java
+++ app/src/main/java/com/dafa/hyperpaysdk/core/TestHyperPay3Ds.java
@@ -1,6 +1,6 @@
 package com.dafa.hyperpaysdk.core;
 //import com.oppwa.mobile.connect.provider.Connect;
-public class TestHyperPayMSA {
+public class TestHyperPay3Ds {
 
     public static String getName() { return  "i am still in development hyperpay";  }
 
